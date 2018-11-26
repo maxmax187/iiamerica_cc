e2f9c2554b24588b6b202e6a33f7d37f00b03094
maxmax187 <45132899+maxmax187@users.noreply.github.com>
2018-11-26 20:00:16 +0100

    Add files via upload

---
+++ cc/start.bat
@@ -0,0 +1,6 @@
+START /MIN MinecraftClient.exe username password play.minesaga.org
+timeout 10
+START /MIN MinecraftClient.exe username password play.minesaga.org
+timeout 10
+START /MIN MinecraftClient.exe username password play.minesaga.org
+timeout 10
